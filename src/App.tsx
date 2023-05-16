a629bae39408ea1bf7230e7a6223aa9caa0b5615
vipulc8 <vipul.c@simformsolutions.com>
2023-05-16 05:54:46 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <>
+      <div className='flex sm:justify-around justify-center'>
+          <div>
+            signup area
+          </div>
+          <div className="hidden sm:block">
+            image area
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
